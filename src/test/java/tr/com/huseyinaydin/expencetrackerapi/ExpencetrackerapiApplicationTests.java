045d17f876dde92156a91c9613b30c1451a74b53
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-09-26 21:53:17 +0300

    Spring Boot + Security + Data JPA + JWT + MySQL + Postman teknolojileri kullanılmıştır. Çok katmanlı mimariye sahiptir. ORM + IoC + DI + Strategy Desing Pattern + Repository Desing Pattern teknikleri kullanılmıştır. Harcama yöneticisi WebAPI projesidir. Kullanıcı register olduktan sonra login olmak için postman üzerinden email ve şifresini girer. Sonrasında ise bir JSON Web Token elde eder. Bu token onun erişim anahtarıdır(jeton). Bu jeton'nun içerisinde kullanıcının kimlik bilgileri, yetkileri ve geçerlilik süresi vardır. Belirlenen geçerlilik süresinde bu jeton ile sisteme erişim sağlar. Yapmış olduğu harcamaları CRUD operasyonları ile yönetir. Kendi profilini güncelleyebilir ve deactive edebilir. Deactive işlemi aslında kullanıcıyı komple veritabanından silmektedir.

--- src/test/java/tr/com/huseyinaydin/expencetrackerapi/ExpencetrackerapiApplicationTests.java
+++ src/test/java/tr/com/huseyinaydin/expencetrackerapi/ExpencetrackerapiApplicationTests.java
@@ -3,11 +3,20 @@ package tr.com.huseyinaydin.expencetrackerapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//بسم الله الرحمن الرحيم
+
+/**
+ *
+ * @author Huseyin_Aydin
+ * @since 1994
+ * @category Java, Spring Boot.
+ *
+ */
+
 @SpringBootTest
 class ExpencetrackerapiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
